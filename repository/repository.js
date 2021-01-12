94fd42e901481a1238b6a33f77f3af7d7c48cc50
R <r@Rs-MacBook-Air.local>
2021-01-12 15:04:15 -0800

    Add code to implement getCustomer method

--- repository/repository.js
+++ repository/repository.js
@@ -216,7 +216,15 @@ class Repository {
        * Lab 1:  K/V operation - Get
        *  1.  Get customer:  bucket.get(key)
        */
-      callback(null, "NOP");
+      this.bucket.get(customerId, function(err, result) {
+        if (!err) {
+          let customer = result.value;
+          callback(err, customer);
+        } else {
+          callback(err, null);
+        }
+      });
+      // callback(null, "NOP");
     } catch (err) {
       //Optional - add business logic to handle error types
       outputMessage(err, "repository.js:getCustomer() - error:");
